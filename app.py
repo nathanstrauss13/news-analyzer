20944e58ad9d615d52190c289579000581eed295
nathanstrauss13 <nathanstrauss@gmail.com>
2025-02-16 14:06:04 -0500

    Fix Heroku port binding

--- app.py
+++ app.py
@@ -196,5 +196,7 @@ def index():
             
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    if __name__ == "__main__":
+    # Get the port from the environment variable or use 5001 as default
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
